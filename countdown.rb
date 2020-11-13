86625bed037f7a587a1f25668a34ff371fe1517a
Loren Cipriano <loren25cipriano@gmail.com>
2020-11-13 12:44:50 -0800

    commit tree 8bf25b95c8796e1ffe4ec2054530bb143c91690f

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,27 @@
 #write your code here
 
-def countdown
+def countdown(seconds)
+    while seconds > 0
+        puts "#{seconds} SECOND(S)!"
+        seconds -= 1
+    end 
+
+    "HAPPY NEW YEAR!"
 end
+
+
+def countdown_with_sleep(seconds)
+    while seconds > 0
+        puts "#{seconds} SECOND(S)!"
+        sleep(5)
+        seconds -= 1
+    end 
+end 
+
+
+
+
+# #countdown
+# outputs '<n> SECOND(S)!' string to STDOUT for each count
+# #countdown_with_sleep
+# should take at least 5 seconds to execute
